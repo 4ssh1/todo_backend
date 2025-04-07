0f32b0668f26cc71640d635c0cc7180d12104a9a
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-07 07:20:08 +0100

    removed the preflight logic

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const cookie = require('cookie-parser')
 const app = express();
-app.use(cookie())
 
 const {todoRouter} = require('./routes/todoRoutes')
 const {userRouter} = require('./routes/userRoute')
@@ -15,21 +14,16 @@ const quoteFunc = quoteslib.getRandomQuotes;
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.DATABASE)
-        .then(()=>console.log("DB connection is successful"))
-        .catch((err)=>console.log("DB error: ", err))
+.then(()=>console.log("DB connection is successful"))
+.catch((err)=>console.log("DB error: ", err))
 
 app.use(cors({
     origin: "https://todo-list-hgia.vercel.app",
     credentials: true
 }));
 
-// Handle preflight requests for all routes
+app.use(cookie())
 
-app.options("*", cors({
-    origin: "https://todo-list-hgia.vercel.app",
-    credentials: true
-  }));
-  
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/todo', todoRouter)
